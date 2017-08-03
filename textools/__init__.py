0ecce2c0f83cf63b43e155d9d18b260cd03c8708
renderhjs <renderhjs@gmail.com>
2017-08-03 13:46:20 +1000

    Initial python folder structure and draft scripts

---
+++ textools/__init__.py
@@ -0,0 +1,131 @@
+#    Blender TexTools, 
+#    <TexTools, Blender addon for editing UVs and Texture maps.>
+#    Copyright (C) <2017> <renderhjs>
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name": "TexTools",
+    "description": "UV and texture Tools for Blender. Based on ideas of the original TexTools for 3dsMax.",
+    "author": "renderhjs",
+    "version": (1, 0, 0),
+    "blender": (2, 71, 0),
+    "category": "UV",
+    #"location": "UV Image Editor > UVs > UVs to grid of squares",
+    #"warning": "",
+#    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/UV/Uv_Squares"
+    }
+
+
+import bpy
+import bmesh
+
+
+
+class SortAndPack(bpy.types.Operator):
+    #Sorts shapes
+    bl_idname = "uv.textools_sort_and_pack"
+    bl_label = "Rotates UV islands to minimal bounds and sorts them horizontal or vertical"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return (context.mode == 'EDIT_MESH')
+    
+    def execute(self, context):
+        #main(context, self)
+        return {'FINISHED'}
+
+
+class TexToolsPanel(bpy.types.Panel):
+#    """TexTools Panel"""
+    bl_label = "TexTools"
+    bl_space_type = 'IMAGE_EDITOR'
+    bl_region_type = 'TOOLS'
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text="Prototype scripts area")
+        
+        split = layout.split()
+        col = split.column(align=True)
+        #col.operator(UvSquaresByShape.bl_idname, text="To Grid By Shape", icon = "GRID")
+        col.operator(SortAndPack.bl_idname, text="Sort And Pack", icon = "GRID")
+        
+
+        row = layout.row()
+        row.label(text="UV Island")
+        split = layout.split()
+        col = split.column(align=True)
+        col.operator(SortAndPack.bl_idname, text="Align by Edge")
+        col.operator(SortAndPack.bl_idname, text="Align LT,RT,TP,BM")
+        col.operator(SortAndPack.bl_idname, text="Rotate 90")
+        col.operator(SortAndPack.bl_idname, text="Sort & Align")
+        
+        row = layout.row()
+        row.label(text="Misc")
+        split = layout.split()
+        col = split.column(align=True)
+        col.operator(SortAndPack.bl_idname, text="Islands to Smoothing Groups")
+        col.operator(SortAndPack.bl_idname, text="Symmetry")
+        col.operator(SortAndPack.bl_idname, text="Align Island to Edge")
+        
+        
+        
+#        col.label(text="Islands to Smoothing Groups")
+
+#        row.label(text="Prototype scripts area")
+#        row = layout.row()
+#        row.label(text="Align Island to Edge")
+#        row = layout.row()
+#        row.label(text="Align Islands LT,RT,TP,BM")
+#        row = layout.row()
+#        row.label(text="Rotate Islands 90")
+#        row = layout.row()
+#        row.label(text="Symmetry")
+#        row = layout.row()
+#        row.label(text="Sort")
+        
+        
+    
+def register():
+    bpy.utils.register_class(TexToolsPanel)
+    bpy.utils.register_class(SortAndPack)
+    #menu
+    #bpy.types.IMAGE_MT_uvs.append(menu_func_uv_squares)
+   
+
+    #handle the keymap
+#    wm = bpy.context.window_manager
+    
+#    km = wm.keyconfigs.addon.keymaps.new(name='UV Editor', space_type='EMPTY')
+#    kmi = km.keymap_items.new(UvSquaresByShape.bl_idname, 'E', 'PRESS', alt=True)
+#    addon_keymaps.append((km, kmi))
+    
+    
+
+def unregister():
+    bpy.utils.unregister_class(TexToolsPanel)
+    bpy.utils.unregister_class(SortAndPack)
+#    bpy.types.IMAGE_MT_uvs.remove(menu_func_uv_squares)
+    
+    # handle the keymap
+#    for km, kmi in addon_keymaps:
+#        km.keymap_items.remove(kmi)
+    # clear the list
+#    addon_keymaps.clear()
+
+if __name__ == "__main__":
+    register()
---
+++ textools/textools_islandsAlignSort.py
@@ -0,0 +1,205 @@
+import bpy
+import bmesh
+from mathutils import Vector
+from collections import defaultdict
+from math import pi
+
+def CollectUVIslands():
+    print("Collect UV islands")
+    
+    bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
+    uvLayer = bm.loops.layers.uv.verify()
+
+    #Reference A: https://github.com/nutti/Magic-UV/issues/41
+    #Reference B: https://github.com/c30ra/uv-align-distribute/blob/v2.2/make_island.py
+
+    #Extend selection
+    bpy.ops.uv.select_linked(extend=False)
+ 
+    #Collect selected UV faces
+    selectedFaces = [];
+    for face in bm.faces:
+        if face.select == False:
+            continue
+        
+        isUVFaceSelected = True;
+        for loop in face.loops:
+            if loop[uvLayer].select is False:
+                isUVFaceSelected = False;
+                continue
+                
+        if isUVFaceSelected == True:
+            selectedFaces.append(face)
+            
+    print("Faces: "+str(len(selectedFaces)))
+    
+    #Collect UV islands
+    parsedFaces = []
+    islands = []
+
+    for face in selectedFaces:
+        #Skip if already processed
+        if face in parsedFaces:
+            continue;
+        
+        #Select single face
+        bpy.ops.uv.select_all(action='DESELECT')
+        for loop in face.loops:
+            loop[uvLayer].select = True;
+        bpy.ops.uv.select_linked(extend=False)#Extend selection
+        
+        #Collect faces
+        islandFaces = [];
+        for faceAll in bm.faces:
+            if faceAll.select == False or faceAll in parsedFaces:
+                continue
+            isUVFaceSelected = True;
+            for loop in faceAll.loops:
+                if loop[uvLayer].select is False:
+                    isUVFaceSelected = False;
+                    continue
+                    
+            if isUVFaceSelected == True:
+                islandFaces.append(faceAll)
+                #Add to parsed list, to skip next time
+                if faceAll not in parsedFaces:
+                    parsedFaces.append(faceAll)
+                    
+        #Assign Faces to island
+        islands.append(islandFaces)
+    
+    #Restore selection
+    for face in selectedFaces:
+        for loop in face.loops:
+            loop[uvLayer].select = True
+
+    
+    print("Islands: "+str(len(islands))+"x")
+    return islands
+        
+
+
+def GetSelectionBBox():
+    bm = bmesh.from_edit_mesh(bpy.context.active_object.data);
+    uvLayer = bm.loops.layers.uv.verify();
+    
+    bbox = {}
+    
+    boundsMin = Vector((99999999.0,99999999.0))
+    boundsMax = Vector((-99999999.0,-99999999.0))
+    boundsCenter = Vector((0.0,0.0))
+    countFaces = 0;
+    
+    for face in bm.faces:
+        if face.select == True:
+            for loop in face.loops:
+                if loop[uvLayer].select is True:
+                    uv = loop[uvLayer].uv
+                    boundsMin.x = min(boundsMin.x, uv.x)
+                    boundsMin.y = min(boundsMin.y, uv.y)
+                    boundsMax.x = max(boundsMax.x, uv.x)
+                    boundsMax.y = max(boundsMax.y, uv.y)
+            
+                    boundsCenter+= uv
+                    countFaces+=1
+    
+    bbox['min'] = boundsMin
+    bbox['max'] = boundsMax
+    bbox['center'] = boundsCenter / countFaces
+    bbox['minLength'] = (boundsMax - boundsMin).x * (boundsMax - boundsMin).y 
+                
+    return bbox;
+    
+
+def AlignIslandMinimalBounds(uvLayer, faces):
+    print("Align island")
+    
+    
+    
+    
+    #Select Island
+    bpy.ops.uv.select_all(action='DESELECT')
+
+    for face in faces:
+        for loop in face.loops:
+            loop[uvLayer].select = True
+
+
+    iterations = 4
+    steps = 8
+    angle = 90 / steps
+    
+    lengthA = 0
+    lengthB = 0
+    
+    for i in range(0, iterations):
+        
+        angleBest = 0
+        
+        bbox = GetSelectionBBox()
+        lengthA = bbox['minLength']
+        
+        for j in range(1, steps-1):
+            bpy.ops.transform.rotate(value=(angle * pi / 180), axis=(-0, -0, -1))
+        
+            bbox = GetSelectionBBox()
+            lengthB = bbox['minLength']
+            if lengthB < lengthA:
+                lengthA = lengthB;
+                angleBest = j*angle
+        
+        angleCorrection = angleBest - angle*(steps-1)
+        #if i != 
+        
+        bpy.ops.transform.rotate(value=(angleCorrection * pi / 180), axis=(-0, -0, -1))
+            
+            
+        #print("BBox Size "+str(bbox['minLength']))    
+    
+    print("Angle: "+str(angle))
+    
+    
+
+
+
+
+def SortAndPack():
+    #Only in Edit mode
+    if bpy.context.active_object.mode != 'EDIT':
+        return
+
+    #Requires UV map
+    if not bpy.context.object.data.uv_layers:
+         #self.report({'WARNING'}, "Object must have more than one UV map")
+        return {'CANCELLED'}
+    
+    #Only in UV editor mode
+    lastOperator = bpy.context.area.type;
+    if bpy.context.area.type != 'IMAGE_EDITOR':
+        bpy.context.area.type = 'IMAGE_EDITOR'
+    
+    #not in Synced mode
+    if bpy.context.scene.tool_settings.use_uv_select_sync == False:
+        #Only in Face or Island mode
+        if bpy.context.scene.tool_settings.uv_select_mode is not 'FACE' or 'ISLAND':
+            bpy.context.scene.tool_settings.uv_select_mode = 'FACE'
+    
+        #Select all linked islands
+        #bpy.ops.uv.select_linked(extend=False)
+        
+        bm = bmesh.from_edit_mesh(bpy.context.active_object.data);
+        uvLayer = bm.loops.layers.uv.verify();
+        
+        islands = CollectUVIslands()
+        
+        for island in islands:
+            AlignIslandMinimalBounds(uvLayer, island)
+    
+    
+#    
+    
+    #restore context, e.g. back to code editor instead of uv editor
+    bpy.context.area.type = lastOperator
+
+
+SortAndPack()

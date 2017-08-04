bcd16c76395eb72856689bcc72228e3641a96fd4
renderhjs <renderhjs@gmail.com>
2017-08-05 00:39:32 +1000

    Checker Map operator Added

--- addons/textools/__init__.py
+++ addons/textools/__init__.py
@@ -15,85 +15,76 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name": "TexTools",
-    "description": "UV and texture Tools for Blender. Based on ideas of the original TexTools for 3dsMax.",
-    "author": "renderhjs",
-    "version": (1, 0, 0),
-    "blender": (2, 71, 0),
-    "category": "UV",
-    "location": "UV Image Editor > UVs > UVs to grid of squares",
-    "warning": "",
-    "wiki_url": "http://renderhjs.net/textools/"
+	"name": "TexTools",
+	"description": "UV and texture Tools for Blender. Based on ideas of the original TexTools for 3dsMax.",
+	"author": "renderhjs",
+	"version": (1, 0, 0),
+	"blender": (2, 71, 0),
+	"category": "UV",
+	"location": "UV Image Editor > UVs > UVs to grid of squares",
+	"warning": "",
+	"wiki_url": "http://renderhjs.net/textools/"
 }
 
 # Import local modules
 if "bpy" in locals():
-    import imp
-    imp.reload(operator_islandsAlignSort)
+	import imp
+	imp.reload(operator_islandsAlignSort)
+	imp.reload(operator_checkerMap)
 else:
-    from . import operator_islandsAlignSort
+	from . import operator_islandsAlignSort
+	from . import operator_checkerMap
+	
 
 # Import general modules. Important: must be placed here and not on top
 import bpy
 import os
-# import bmesh
 import bpy.utils.previews
-
-
-class SortAndPack(bpy.types.Operator):
-    #Sorts shapes
-    bl_idname = "uv.textools_sort_and_pack"
-    bl_label = "Rotates UV islands to minimal bounds and sorts them horizontal or vertical"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    @classmethod
-    def poll(cls, context):
-        return (context.mode == 'EDIT_MESH')
-    
-    def execute(self, context):
-        #main(context, self)
-        return {'FINISHED'}
 
 
 class TexToolsPanel(bpy.types.Panel):
 #    """TexTools Panel"""
-    bl_label = "TexTools"
-    bl_space_type = 'IMAGE_EDITOR'
-    bl_region_type = 'TOOLS'
-    
-    def draw(self, context):
-        layout = self.layout
-        
-        row = layout.row()
-        row.label(text="Prototype scripts area")
-        
-        split = layout.split()
-        col = split.column(align=True)
-        #col.operator(UvSquaresByShape.bl_idname, text="To Grid By Shape", icon = "GRID")
-        # col.operator(SortAndPack.bl_idname, text="Sort & Pack", icon = "GRID")
-        
-        col.operator(operator_islandsAlignSort.IslandsAlignSort.bl_idname, text="Align & Sort", icon_value = preview_icons["islandsAlignSort"].icon_id)
-        
+	bl_label = "TexTools"
+	bl_space_type = 'IMAGE_EDITOR'
+	bl_region_type = 'TOOLS'
+	
+	def draw(self, context):
+		layout = self.layout
+		
+		row = layout.row()
+		row.label(text="Prototype scripts area")
+		
+		split = layout.split()
+		col = split.column(align=True)
+		#col.operator(UvSquaresByShape.bl_idname, text="To Grid By Shape", icon = "GRID")
+		# col.operator(SortAndPack.bl_idname, text="Sort & Pack", icon = "GRID")
+		
+		col.operator(operator_islandsAlignSort.IslandsAlignSort.bl_idname, text="Align & Sort", icon_value = preview_icons["islandsAlignSort"].icon_id)
+		col.operator(operator_checkerMap.CheckerMap.bl_idname, text="Checker Map", icon_value = preview_icons["islandsAlignSort"].icon_id)
+		
+		
 
-        # row = layout.row()
-        # row.label(text="UV Island")
-        # split = layout.split()
-        # col = split.column(align=True)
-        # col.operator(SortAndPack.bl_idname, text="Align by Edge")
-        # col.operator(SortAndPack.bl_idname, text="Align LT,RT,TP,BM")
-        # col.operator(SortAndPack.bl_idname, text="Rotate 90")
-        # col.operator(SortAndPack.bl_idname, text="Sort & Align")
-        
-        # row = layout.row()
-        # row.label(text="Misc")
-        # split = layout.split()
-        # col = split.column(align=True)
-        # col.operator(SortAndPack.bl_idname, text="Islands to Smoothing Groups")
-        # col.operator(SortAndPack.bl_idname, text="Symmetry")
-        # col.operator(SortAndPack.bl_idname, text="Align Island to Edge")
-        
-        
-        
+
+
+		# row = layout.row()
+		# row.label(text="UV Island")
+		# split = layout.split()
+		# col = split.column(align=True)
+		# col.operator(SortAndPack.bl_idname, text="Align by Edge")
+		# col.operator(SortAndPack.bl_idname, text="Align LT,RT,TP,BM")
+		# col.operator(SortAndPack.bl_idname, text="Rotate 90")
+		# col.operator(SortAndPack.bl_idname, text="Sort & Align")
+		
+		# row = layout.row()
+		# row.label(text="Misc")
+		# split = layout.split()
+		# col = split.column(align=True)
+		# col.operator(SortAndPack.bl_idname, text="Islands to Smoothing Groups")
+		# col.operator(SortAndPack.bl_idname, text="Symmetry")
+		# col.operator(SortAndPack.bl_idname, text="Align Island to Edge")
+		
+		
+		
 #        col.label(text="Islands to Smoothing Groups")
 
 #        row.label(text="Prototype scripts area")
@@ -111,54 +102,53 @@ class TexToolsPanel(bpy.types.Panel):
 
 
 def registerIcon(fileName):
-    # global custom_icons
-    print("---> Register: "+fileName.split('.')[0])
-    name = fileName.split('.')[0]
-    icons_dir = os.path.join(os.path.dirname(__file__), "icons")
-    preview_icons.load(name, os.path.join(icons_dir, fileName), 'IMAGE')
-    
+	# global custom_icons
+	name = fileName.split('.')[0]   # Don't include file extension
+	icons_dir = os.path.join(os.path.dirname(__file__), "icons")
+	preview_icons.load(name, os.path.join(icons_dir, fileName), 'IMAGE')
+	
 def register():
-    # Register global Icons
-    global preview_icons
-    preview_icons = bpy.utils.previews.new()
-    registerIcon("islandsAlignSort.png")
+	# Register global Icons
+	global preview_icons
+	preview_icons = bpy.utils.previews.new()
+	registerIcon("islandsAlignSort.png")
 
-    # Register Operators
-    bpy.utils.register_class(TexToolsPanel)
-    bpy.utils.register_class(SortAndPack)
-    bpy.utils.register_class(operator_islandsAlignSort.IslandsAlignSort)
+	# Register Operators
+	bpy.utils.register_class(TexToolsPanel)
+	bpy.utils.register_class(operator_islandsAlignSort.IslandsAlignSort)
+	bpy.utils.register_class(operator_checkerMap.CheckerMap)
 
-    #handle the keymap
+	#handle the keymap
 #    wm = bpy.context.window_manager
-    
+	
 #    km = wm.keyconfigs.addon.keymaps.new(name='UV Editor', space_type='EMPTY')
 #    kmi = km.keymap_items.new(UvSquaresByShape.bl_idname, 'E', 'PRESS', alt=True)
 #    addon_keymaps.append((km, kmi))
-    
-    
+	
+	
 
 def unregister():
-    # Unregister icons
-    global preview_icons
-    bpy.utils.previews.remove(preview_icons)
+	# Unregister icons
+	global preview_icons
+	bpy.utils.previews.remove(preview_icons)
 
-    #Unregister Operators
-    bpy.utils.unregister_class(TexToolsPanel)
-    bpy.utils.unregister_class(SortAndPack)
-    bpy.utils.unregister_class(operator_islandsAlignSort.IslandsAlignSort)
+	#Unregister Operators
+	bpy.utils.unregister_class(TexToolsPanel)
+	bpy.utils.unregister_class(operator_islandsAlignSort.IslandsAlignSort)
+	bpy.utils.unregister_class(operator_checkerMap.CheckerMap)
 
 
-    bpy.utils.unregister_module(__name__)
+	bpy.utils.unregister_module(__name__)
 
 
 
 #    bpy.types.IMAGE_MT_uvs.remove(menu_func_uv_squares)
-    
-    # handle the keymap
+	
+	# handle the keymap
 #    for km, kmi in addon_keymaps:
 #        km.keymap_items.remove(kmi)
-    # clear the list
+	# clear the list
 #    addon_keymaps.clear()
 
 if __name__ == "__main__":
-    register()
+	register()
---
+++ addons/textools/operator_checkerMap.py
@@ -0,0 +1,37 @@
+import bpy
+import bmesh
+from mathutils import Vector
+from collections import defaultdict
+from math import pi
+
+
+
+def main(context):
+	print("Executing IslandsAlignSort main")
+   
+	
+
+class CheckerMap(bpy.types.Operator):
+	"""UV Operator description"""
+	bl_idname = "uv.textools_checker_map"
+	bl_label = "Assign Checker Map"
+
+	@classmethod
+	def poll(cls, context):
+		return True
+
+	def execute(self, context):
+		main(context)
+		return {'FINISHED'}
+
+
+if __name__ == "__main__":
+ 	# test call
+	lastOperator = bpy.context.area.type;
+	if bpy.context.area.type != 'IMAGE_EDITOR':
+		bpy.context.area.type = 'IMAGE_EDITOR'
+
+	bpy.ops.uv.textools_checker_map()
+
+	#restore context, e.g. back to code editor instead of uv editor
+	bpy.context.area.type = lastOperator

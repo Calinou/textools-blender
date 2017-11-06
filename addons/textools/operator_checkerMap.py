6057eaa6cac01598e7885c075a66bd7d8c080a40
hendrik <hendrik.page2@gmail.com>
2017-11-06 16:54:51 +1100

    checkermap button now loads and assigns checker map view on selected objects

--- addons/textools/operator_checkerMap.py
+++ addons/textools/operator_checkerMap.py
@@ -1,15 +1,9 @@
 import bpy
+import os
 import bmesh
 from mathutils import Vector
 from collections import defaultdict
 from math import pi
-
-
-
-def main(context):
-	print("Executing operator_checkerMap main()")
-   
-	
 
 class operator_checkerMap(bpy.types.Operator):
 	"""UV Operator description"""
@@ -24,6 +18,41 @@ class operator_checkerMap(bpy.types.Operator):
 	def execute(self, context):
 		main(context)
 		return {'FINISHED'}
+
+
+def main(context):
+	print("Executing operator_checkerMap main()")
+
+	idImage = "TT_checkerMap"
+	image = None
+
+	#Get Image
+	if bpy.data.images.get(idImage) is not None:
+		#Already exists
+  		image = bpy.data.images[idImage];
+	else:
+		#Load image
+		pathTexture = icons_dir = os.path.join(os.path.dirname(__file__), "resources/checkerMap.jpg")
+		image = bpy.ops.image.open(filepath=pathTexture, relative_path=False)
+		#Rename
+		bpy.data.images["checkerMap.jpg"].name = idImage
+	
+	#Assign image variable
+	image = bpy.data.images[idImage];
+
+	#Assign image to UV faces
+	for obj in context.selected_objects:
+		#Only Assign if already UV maps
+		if obj.data.uv_layers:
+			for uv_face in obj.data.uv_textures.active.data:
+				uv_face.image = image
+
+	#Change View mode to TEXTURED
+	for area in bpy.context.screen.areas: # iterate through areas in current screen
+		if area.type == 'VIEW_3D':
+			for space in area.spaces: # iterate through spaces in current VIEW_3D area
+				if space.type == 'VIEW_3D': # check if space is a 3D view
+					space.viewport_shade = 'TEXTURED' # set the viewport shading to rendered
 
 
 if __name__ == "__main__":
--- snippets/snippet_assignCheckerMap.py
+++ snippets/snippet_assignCheckerMap.py
@@ -3,15 +3,12 @@ import bpy
 # blank image
 #image = bpy.data.images.new("uvChecker", width=512, height=512)
 
-bpy.ops.image.open(filepath="//..\\Documents\\GIT\\TexToolsBlender\\addons\\textools\\resources\\checkerMap.jpg", directory="D:\\Documents\\GIT\\TexToolsBlender\\addons\\textools\\resources\\", files=[{"name":"checkerMap.jpg", "name":"checkerMap.jpg"}], relative_path=True, show_multiview=False)
+#bpy.ops.image.open(filepath="//..\\Documents\\GIT\\TexToolsBlender\\addons\\textools\\resources\\checkerMap.jpg", directory="D:\\Documents\\GIT\\TexToolsBlender\\addons\\textools\\resources\\", files=[{"name":"checkerMap.jpg", "name":"checkerMap.jpg"}], relative_path=True, show_multiview=False)
 
+idImage = "TT_checkerMap"
 
-
-for area in bpy.context.screen.areas:
-    print("..>>"+ area.type)
-    for space in area.spaces:
-            if space.type == 'VIEW_3D':
-                spaceviewpoert_shade = 'TEXTURED'
+if bpy.data.images.get(idImage) is not None:
+    print("Image found")
 
 #Create 
 #bpy.data.images["uvChecker"].name = "uvChecker"

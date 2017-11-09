3f162d7d8799bd6acdf96bbf71b11e23857db89f
renderhjs <renderhjs@gmail.com>
2017-11-09 23:50:05 +1100

    copy script snippet now functional

--- snippets/snippet_collapseSelectedObjects.py
+++ snippets/snippet_collapseSelectedObjects.py
@@ -1,6 +1,19 @@
+import bpy
 
-    
-    
-    # then apply them individually.
-    for modifier in target_obj.modifiers:
-        bpy.ops.object.modifier_apply(modifier=modifier.name)
+for obj in bpy.context.selected_objects:
+    if obj.type == 'MESH':
+        print("Copy Mesh: "+obj.name)
+        
+        #Create copy
+        copy = obj.copy()
+        copy.data = obj.data.copy()
+        copy.animation_data_clear()
+        bpy.context.scene.objects.link(copy)
+        bpy.context.scene.update()
+        
+        bpy.ops.object.select_all(action='DESELECT')
+        copy.select = True
+        copy.name = "merged__"
+        
+        bpy.context.scene.objects.active = copy
+        bpy.ops.object.convert()#target='MESH'

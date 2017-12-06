3cadbaa3981c0ac0dfcf73d34ef29247ec0d8f32
renderhjs <renderhjs@gmail.com>
2017-12-07 01:51:17 +1100

    symmetry: - aligns cluster to average angle of symmetry edge line - when faces selected symmetry edge line is now calculated

--- addons/textools/op_symmetry.py
+++ addons/textools/op_symmetry.py
@@ -1,9 +1,9 @@
 import bpy
 import os
 import bmesh
+import math
 from mathutils import Vector
 from collections import defaultdict
-from math import pi
 
 from . import utilities_uv
 
@@ -40,6 +40,46 @@ class op(bpy.types.Operator):
 		return {'FINISHED'}
 
 
+def alignToCenterLine():
+	print("align to center line")
+
+	bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
+	uvLayer = bm.loops.layers.uv.verify()
+	bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE')
+
+	# 1.) Get average edges rotation + center
+	average_angle = 0
+	average_center = Vector((0,0))
+	average_count = 0
+	for face in bm.faces:
+		if face.select:
+			verts = []
+			for loop in face.loops:
+				if loop[uvLayer].select:
+					verts.append(loop[uvLayer].uv)
+
+			if len(verts) == 2:
+				diff = verts[1] - verts[0]
+				angle = math.atan2(diff.y, diff.x)%(math.pi)
+				average_center += verts[0] + diff/2
+				average_angle += angle
+				average_count+=1
+
+	if average_count >0:
+		average_angle/=average_count
+		average_center/=average_count
+
+	average_angle-= math.pi/2 #Rotate -90 degrees so aligned horizontally
+
+	# 2.) Rotate UV Shell around edge
+	bpy.context.space_data.pivot_point = 'CURSOR'
+	bpy.ops.uv.cursor_set(location=average_center)
+
+	bpy.ops.uv.select_linked(extend=False)
+	bpy.ops.transform.rotate(value=average_angle, axis=(0, 0, -1), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED')
+
+
+
 def main(context):
 	print("Executing operator_symmetry")
 
@@ -49,8 +89,103 @@ def main(context):
 	bm = bmesh.from_edit_mesh(bpy.context.active_object.data)
 	uvLayer = bm.loops.layers.uv.verify()
 
+	if bpy.context.scene.tool_settings.uv_select_mode == 'EDGE':
+		# 1.) Align UV shell
+		alignToCenterLine()
+
+
 	if bpy.context.scene.tool_settings.uv_select_mode == 'FACE':
-		print( "1.) Get Face edge bounds selection")
+
+		# 1.) Get selected UV faces to vert faces
+		selected_faces = []
+		for face in bm.faces:
+			if face.select:
+				# Are all UV faces selected?
+				countSelected = 0
+				for loop in face.loops:
+					if loop[uvLayer].select:
+						countSelected+=1
+						# print("Vert selected "+str(face.index))
+				if countSelected == len(face.loops):
+					selected_faces.append(face)
+
+		bpy.ops.mesh.select_all(action='DESELECT')
+		for face in selected_faces:
+			face.select = True
+
+
+		# 2.) Select Vert shell's outer edges
+		bpy.ops.mesh.select_linked(delimit=set())
+		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE')
+		bpy.ops.mesh.region_to_loop()
+		edges_outer_shell = [e for e in bm.edges if e.select]
+
+		# 3.) Select Half's outer edges
+		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='FACE')
+		bpy.ops.mesh.select_all(action='DESELECT')
+		for face in selected_faces:
+			face.select = True
+		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE')
+		bpy.ops.mesh.region_to_loop()
+		edges_outer_selected = [e for e in bm.edges if e.select]
+
+		# 4.) Mask edges exclusive to edges_outer_selected (symmetry line)
+		edges_middle = [item for item in edges_outer_selected if item not in edges_outer_shell]
+
+		# 5.) Convert to UV selection
+		verts_middle = []
+		for edge in edges_middle:
+			if edge.verts[0] not in verts_middle:
+				verts_middle.append(edge.verts[0])
+			if edge.verts[1] not in verts_middle:
+				verts_middle.append(edge.verts[1])
+
+		#Select all Vert shell faces
+		bpy.ops.mesh.select_linked(delimit=set())
+		#Select UV matching vert array
+		bpy.ops.uv.select_all(action='DESELECT')
+		for face in bm.faces:
+			if face.select:
+				for loop in face.loops:
+					if loop.vert in verts_middle:
+						loop[uvLayer].select = True
+
+		# 5.) Align UV shell
+		alignToCenterLine()
+
+		# 6.) Restore UV face selection
+		bpy.ops.uv.select_all(action='DESELECT')
+		for face in selected_faces:
+			for loop in face.loops:
+				loop[uvLayer].select = True
+
+
+		# bpy.ops.mesh.select_all(action='DESELECT')
+		# for edge in edges_middle:
+		# 	edge.select = True
+
+		# 5.) Convert to UV selection
+
+		
+
+'''
+
+
+		#Select all and get outer edges
+		bpy.ops.mesh.select_all(action='SELECT')
+		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE')
+		bpy.ops.mesh.region_to_loop()
+
+
+'''
+
+
+
+
+
+
+
+'''
 
 		# Get selected UV faces
 		selected_faces = []
@@ -74,6 +209,14 @@ def main(context):
 		bpy.ops.mesh.select_mode(use_extend=False, use_expand=False, type='EDGE')
 		bpy.ops.mesh.region_to_loop()
 
+		selected_edges = [e for e in bm.edges if e.select]
+		print("edges "+str(len(selected_edges)))		
+'''
+
+
+
+
+'''
 		selected_verts = [v for v in bm.verts if v.select]
 
 		print("verts "+str(len(selected_verts)))
@@ -93,3 +236,4 @@ def main(context):
 					print("Yes "+str(vert.index))
 					loop[uvLayer].select = True
 				# loop[uv_layer].uv = get_uvs(vert.index, face.index,...)
+'''

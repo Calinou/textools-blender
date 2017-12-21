87c230268fa6e715d01f58eda015a1eb2ab80681
renderhjs <renderhjs@gmail.com>
2017-12-22 01:18:44 +1100

    op_checkerMap is now functional

--- addons/textools/op_checkerMap.py
+++ addons/textools/op_checkerMap.py
@@ -40,6 +40,32 @@ def main(context):
 	#Assign image variable
 	image = bpy.data.images[idImage];
 
+	# bpy.context.object.active_material.use_shadeless = True
+	mat = bpy.data.materials.new("TT_checkerMap_material")
+	mat.diffuse_color = (0,0,0)
+	mat.diffuse_shader = 'LAMBERT' 
+	mat.diffuse_intensity = 1.0 
+	mat.specular_color = (1,1,1)
+	mat.specular_shader = 'COOKTORR'
+	mat.specular_intensity = 0.5
+	# mat.alpha = alpha
+	mat.ambient = 1
+	mat.use_shadeless = True
+
+	cTex = bpy.data.textures.new('ColorTex', type = 'IMAGE')
+	cTex.image = image
+
+	# Add texture slot for color texture
+	mtex = mat.texture_slots.add()
+	mtex.texture = cTex
+	mtex.texture_coords = 'UV'
+	mtex.use_map_color_diffuse = True 
+	mtex.use_map_color_emission = True 
+	mtex.emission_color_factor = 0.5
+	mtex.use_map_density = True 
+	mtex.mapping = 'FLAT' 
+
+
 	#Assign image to UV faces
 	for obj in context.selected_objects:
 		#Only Assign if already UV maps
@@ -47,9 +73,12 @@ def main(context):
 			for uv_face in obj.data.uv_textures.active.data:
 				uv_face.image = image
 
+		obj.active_material = mat
+
 	#Change View mode to TEXTURED
 	for area in bpy.context.screen.areas: # iterate through areas in current screen
 		if area.type == 'VIEW_3D':
 			for space in area.spaces: # iterate through spaces in current VIEW_3D area
 				if space.type == 'VIEW_3D': # check if space is a 3D view
 					space.viewport_shade = 'TEXTURED' # set the viewport shading to rendered
+

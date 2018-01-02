e1a212d6e410bf6d070cf112fc14058143a78150
renderhjs <renderhjs@gmail.com>
2018-01-03 01:02:00 +1100

    op_bake: refactored setup_material() code. Created new bake node in cycles node editor and set to active.

--- addons/textools/op_bake.py
+++ addons/textools/op_bake.py
@@ -9,35 +9,21 @@ from . import settings
 from . import utilities_bake
 
 # Get last-loaded material, such as ~.002.
-def _getAppendedMaterial(material_name):
-    # Get material name list.
-    material_names = [m.name for m in bpy.data.materials if material_name in m.name]
-    # Return last material in the sorted order.
-    material_names.sort()
-    return material_names[-1]
+# def _getAppendedMaterial(material_name):
+#     # Get material name list.
+#     material_names = [m.name for m in bpy.data.materials if material_name in m.name]
+#     # Return last material in the sorted order.
+#     material_names.sort()
+#     return material_names[-1]
 
-def getMaterial(materialType):
-	print("Get material of type: '"+str(materialType)+"'")
-
-	name = "textools_bake_pointiness"
+def getMaterial(name):
 	path = os.path.join(os.path.dirname(__file__), "resources/materials.blend")+"\\Material\\"
 
 	if bpy.data.materials.get(name) is None:
-		print("Image not yet loaded: "+name)
+		print("Material not yet loaded: "+name)
 		bpy.ops.wm.append(filename=name, directory=path, link=False, autoselect=False)
-	else:
-		print("Image already loaded :)")
 
-	# If material is loaded some times, select the last-loaded material.
-	last_material = _getAppendedMaterial(name)
-	return bpy.data.materials[last_material]
-
-	# Apply Only one material in the material slot.
-	# for m in bpy.context.object.data.materials:
-	# 	bpy.ops.object.material_slot_remove()
-	# bpy.context.object.data.materials.append(mat)
-
-	#return None; #bpy.data.images[name];
+	return bpy.data.materials.get(name)
 
 
 class op_setup_material(bpy.types.Operator):
@@ -53,23 +39,6 @@ class op_setup_material(bpy.types.Operator):
 		execute_setup_material(context)
 		return {'FINISHED'}
 
-class op_bake(bpy.types.Operator):
-	bl_idname = "uv.textools_bake"
-	bl_label = "Bake"
-	bl_description = "Bake selected objects"
-
-	@classmethod
-	def poll(cls, context):
-		if not bpy.context.active_object:
-			return False
-
-		return True
-
-	def execute(self, context):
-		execute_render(self, context)
-		return {'FINISHED'}
-
-
 
 def execute_setup_material(context):
 	print("Executing operator_bake_render main()")
@@ -77,14 +46,56 @@ def execute_setup_material(context):
 	material = getMaterial("something")
 
 
-def execute_render(self, context):
+class op_bake(bpy.types.Operator):
+	bl_idname = "uv.textools_bake"
+	bl_label = "Bake"
+	bl_description = "Bake selected objects"
+
+	@classmethod
+	def poll(cls, context):
+		if len(bpy.context.selected_objects) == 0:
+			return False
+
+		return True
+
+	def execute(self, context):
+		execute_render(self, context, settings.bake_mode)
+		return {'FINISHED'}
+
+
+
+
+def setup_material(obj, mode):
+
+	if mode == 'bake_normal' or mode == 'bake_ao':
+		# No material setup requires
+		return
+
+	if mode == 'bake_worn' or mode == 'bake_cavity' or mode == 'bake_dust':
+		# Setup vertex dirt colors
+		bpy.ops.paint.vertex_color_dirt()
+	
+	# Assign material
+	material = getMaterial(mode)
+	if len(obj.data.materials) == 0:
+		obj.data.materials.append(material)
+	else:
+		obj.data.materials[0] = material
+
+
+def execute_render(self, context, mode):
+
+	if bpy.context.scene.render.engine != 'CYCLES':
+		bpy.context.scene.render.engine = 'CYCLES'
+
 	if bpy.context.object.mode != 'OBJECT':
 		bpy.ops.object.mode_set(mode='OBJECT')
 
 	sets = utilities_bake.get_bake_pairs()
 	bpy.ops.object.select_all(action='DESELECT')
 	
-	print("________________________________\nBake {} sets @{}".format(len(sets), settings.bake_mode))
+	print("________________________________\nBake {}x '{}'".format(len(sets), mode))
+
 
 	for set in sets:
 		# Requires 1+ low poly objects
@@ -92,14 +103,35 @@ def execute_render(self, context):
 			self.report({'ERROR_INVALID_INPUT'}, "No low poly object selected for {}".format(set.name) )
 			return
 
-		# if len(set.objects_high) == 0:
-		# 	# Assign material to lowpoly
-		# 	print("")
-		# else:
-		# 	# Assign material to highpoly
-		# 	print("")
-
 		# Check for UV maps
-		# if len(set.objects_low.data.uv_layers) == 0:
+		for obj in set.objects_low:
+			if len(obj.data.uv_layers) == 0:
+				print("NO UV MAP FOUND FOR {} ".format(obj.name))
+				return
 
+
+
+		# Assign materials
+		if len(set.objects_high) == 0:
+			# Assign material to lowpoly
+			for obj in set.objects_low:
+				setup_material(obj, mode)
+		else:
+			# Assign material to highpoly
+			for obj in set.objects_high:
+				setup_material(obj, mode)
+
+		# BRM BakeUI: https://github.com/leukbaars/BRM-BakeUI/blob/master/BRM_BakeUI.py
+
+		#create bake image and material
+		image = bpy.data.images.new("BakeImage", width=256, height=256) #context.scene.bakeWidth
+
+		# Setup active node for baking
+		tree = set.objects_low[0].data.materials[0].node_tree
+		node = tree.nodes.new("ShaderNodeTexImage")
+		node.select = True
+		node.image = image
+		tree.nodes.active = node
+
+		
 		print("Bake {}".format(set.name))

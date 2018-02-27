a1597f4e6d70d35e00d32c80f5a08ed6e6e445cc
hendrik <hendrik.page2@gmail.com>
2018-02-27 17:11:39 +1100

    testing: supports loading external blend files and continue executing scripts with context

--- addons/textools_testing/tests_panel_size.py
+++ addons/textools_testing/tests_panel_size.py
@@ -16,11 +16,23 @@ tests = []
 
 
 def test_op_uv_size_get():
-	print("A")
-tests.append( utilities.Op_Test('op_uv_size_get', python='op_uv_size_get', blend="fdsfsd", test=test_op_uv_size_get))
+	bpy.ops.object.select_all(action='SELECT')
+	bpy.ops.uv.textools_uv_size_get()
+	size = bpy.context.scene.texToolsSettings.size
+	return size[0] == 128 and size[1] == 32
+tests.append( utilities.Op_Test('op_uv_size_get', python='op_uv_size_get', blend="test_op_uv_size_get", test=test_op_uv_size_get))
 
 
 def test_op_uv_crop():
-	print("B")
+	for area in bpy.context.screen.areas:
+		if area.type == 'IMAGE_EDITOR':
+			# area.spaces[0].image = image
+			print("... {}".format(type(area)))
+
+	bpy.ops.object.select_all(action='SELECT')
+	bpy.ops.object.mode_set(mode='EDIT')
+	override = utilities.get_context_override_uv()
+	bpy.ops.uv.textools_uv_crop(override)
+	return True
 tests.append( utilities.Op_Test('op_uv_crop', python='op_uv_crop', blend="", test=test_op_uv_crop))
 

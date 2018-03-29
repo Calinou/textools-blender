1e8199d8b6fab35b0d21efd85197bb6d7e62b7b9
renderhjs <renderhjs@gmail.com>
2018-03-29 23:40:52 +1100

    - trim operator functional - utilities mesh tex: get uv mesh from deform modifier object

--- addons/textools/op_mesh_texture_trim.py
+++ addons/textools/op_mesh_texture_trim.py
@@ -58,4 +58,13 @@ def trim(self):
 	obj_uv.data.shape_keys.key_blocks["model"].value = 0
 
 	# Apply bool modifier to trim
+	for obj in obj_textures:
+		bpy.ops.object.select_all(action='DESELECT')
+		obj.select = True
+		bpy.context.scene.objects.active = obj
+		# bpy.ops.object.convert(target='MESH')
+
+		bpy.ops.object.modifier_add(type='BOOLEAN')
+		bpy.context.object.modifiers["Boolean"].object = obj_uv
+
 
--- addons/textools/utilities_mesh_texture.py
+++ addons/textools/utilities_mesh_texture.py
@@ -10,9 +10,19 @@ def find_uv_mesh(objects):
 	for obj in objects:
 		# Requires mesh & UV channel
 		if obj and obj.type == 'MESH': # and not obj.data.uv_layers
+
 			if obj.data.shape_keys and len(obj.data.shape_keys.key_blocks) == 2:
 				if "uv" in obj.data.shape_keys.key_blocks and "model" in obj.data.shape_keys.key_blocks:
 					if "Solidify" in obj.modifiers:
 						return obj
+
+			if len(obj.modifiers) > 0:
+				for modifier in obj.modifiers:
+ 					if modifier.type == 'MESH_DEFORM':
+ 						if modifier.object:
+ 							if modifier.object.data.shape_keys and len(modifier.object.data.shape_keys.key_blocks) == 2:
+ 								return modifier.object
+
+
 	return None
 

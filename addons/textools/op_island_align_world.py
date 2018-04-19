280502c0bfe6d7012d3616be380c7048f7195597
hendrik <hendrik.page2@gmail.com>
2018-04-19 16:47:53 +1000

    .

--- addons/textools/op_island_align_world.py
+++ addons/textools/op_island_align_world.py
@@ -75,6 +75,7 @@ def main(context):
 
 	print("Clusters: {}x".format(len(islands)))
 
+	obj  = bpy.context.object
 
 	for faces in islands:
 		# Get average viewport normal of UV island
@@ -83,14 +84,21 @@ def main(context):
 			avg_normal+=face.normal
 		avg_normal/=len(faces)
 
+		# avg_normal = (obj.matrix_world*avg_normal).normalized()
+
 		# Which Side
+		x = 0
+		y = 1
+		z = 2
 		max_size = max(abs(avg_normal.x), abs(avg_normal.y), abs(avg_normal.z))
 		if(abs(avg_normal.x) == max_size):
 			print("x normal")
 
-			align_island(faces, 'y', 'z')
+			align_island(obj, faces, y, z)
 
 		elif(abs(avg_normal.y) == max_size):
+			print("y normal")
+			align_island(obj, faces, y, z)
 			pass
 		elif(abs(avg_normal.z) == max_size):
 			pass
@@ -102,11 +110,13 @@ def main(context):
 	#Restore selection
 	utilities_uv.selection_restore()
 
-def align_island(faces, s_x='x', s_y='y'):
+def align_island(obj, faces, s_x=0, s_y=1):
 
 	# Find lowest and highest verts
 	minmax_val  = [0,0]
-	minmax_vert [None, None]
+	minmax_vert = [None, None]
+
+	print("faces {}x".format(len(faces)))
 
 	processed = []
 	for face in faces:
@@ -115,12 +125,30 @@ def align_island(faces, s_x='x', s_y='y'):
 				processed.append(vert)
 
 				print("idx {}".format(vert.index))
-				
-				if not minmax_vert[0] or vert[s_y] < minmax_val[0]:
+
+				vert_y = (obj.matrix_world * vert.co)[s_y]
+
+				if not minmax_vert[0]:
+					minmax_vert[0] = vert
+					minmax_val[0] = vert_y
+					continue
+
+				if not minmax_vert[1]:
+					minmax_vert[1] = vert
+					minmax_val[1] = vert_y
+					continue
+
+				if vert_y < minmax_val[0]:
 					# Not yet defined or smaller
 					minmax_vert[0] = vert
-					minmax_val[0] = vert[s_y]
-					break
-				
-				
+					minmax_val[0] = vert_y
+					continue
+
+				elif vert_y > minmax_val[1]:
+					minmax_vert[1] = vert
+					minmax_val[1] = vert_y
+					continue
+
+	if minmax_vert[0] and minmax_vert[1]:
+		print("Min {} , Max {} ".format(minmax_vert[0].index, minmax_vert[1].index))
 				

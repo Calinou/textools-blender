308579204cf4b70bf9390285836d8a8d1998b4b2
renderhjs <renderhjs@gmail.com>
2018-06-14 09:39:39 +1000

    fill tool now enabled in U*I

--- addons/textools/__init__.py
+++ addons/textools/__init__.py
@@ -721,11 +721,7 @@ class Panel_Layout(bpy.types.Panel):
 
 		row = col.row(align=True)
 		row.operator(op_uv_crop.op.bl_idname, text="Crop", icon_value = icon_get("op_uv_crop"))
-
-		if bpy.app.debug_value != 0:
-			c = row.column(align=True)
-			c.alert = True
-			c.operator(op_uv_fill.op.bl_idname, text="Fill", icon_value = icon_get("op_uv_fill"))
+		row.operator(op_uv_fill.op.bl_idname, text="Fill", icon_value = icon_get("op_uv_fill"))
 
 
 		row = col.row(align=True)
